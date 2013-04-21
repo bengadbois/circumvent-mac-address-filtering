2ae58f85b39b754d7b004be8bdf52eb6794ad89f
Dustin Dalen <dustin.dalen@gmail.com>
2013-04-21 12:13:03 -0700

    Added promisc sniffing on tcp as well as using the interface as a command line paramenter

--- circumvent.sh
+++ circumvent.sh
@@ -4,33 +4,38 @@
 # sudo iwlist wlan0 scan
 
 #All info
-if [ $# -eq 0 ];
+if [ $# -lt 2 ];
 then
-	echo Requires one argument as the SSID of the wireless network to connect to
+	echo "Requires two arguments"
+	echo "Arg1 the SSID of the network to connect to"
+	echo "Arg2 the name of the wireless interface"
 	exit
 fi
 
+#sniff for a valid MAC
+# I flag for promisc and e for mac addresses
+# grep for our network ssid then ignore beacons
+sudo tcpdump -Ie -i $2 | grep "$1" | grep -i -v "beacon"
 
-sudo iwconfig wlan0 essid $1
+# join the ssid
+sudo iwconfig $2 essid $1
 
-#TODO sniff for a valid MAC
+#sudo iwconfig $2 $MAC #mac of router to join
 
-sudo iwconfig wlan0 $MAC #mac of router to join
-
-sudo dhclient wlan0
+#sudo dhclient $2
 
 #ping to verify connected, otherwise goto picking a new MAC
 
 
 #Info=$(sudo iwlist wlan0 scan)
-MACAddresses=$(sudo iwlist wlan0 scan essid $1 | grep Address | sed s/Cell\ [0-9]*\ \-\ Address\:\ //)
-MAC=${MACAddresses[@]} | sed s/\ *//
-echo "THE MAC IS " $MAC
+#MACAddresses=$(sudo iwlist wlan0 scan essid $1 | grep Address | sed s/Cell\ [0-9]*\ \-\ Address\:\ //)
+#MAC=${MACAddresses[@]} | sed s/\ *//
+#echo "THE MAC IS " $MAC
 
 #configure before joining
-  sudo iwconfig wlan0 $MAC #mac of router to join
-  sudo iwconfig wlan0 essid $1 #name of network
-  sudo iwconfig wlan0 freq 2.437 #TODO can probably remove
+#  sudo iwconfig wlan0 $MAC #mac of router to join
+#  sudo iwconfig wlan0 essid $1 #name of network
+#  sudo iwconfig wlan0 freq 2.437 #TODO can probably remove
    
 #connect!
-   sudo dhclient wlan0
+#   sudo dhclient wlan0
