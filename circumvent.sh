52928d2ce4091042ef29a5acb7de5f358d5029e6
Dustin Dalen <dustin.dalen@gmail.com>
2013-04-23 23:49:17 -0700

    Got the router mac addr to use in sorting captured traffic

--- circumvent.sh
+++ circumvent.sh
@@ -16,12 +16,18 @@ if [ ! $( id -u ) -eq 0 ]; then
     exit ${?}
 fi
 
+
+#get the mac address of the router given the SSID
+echo "Finding the mac address of the router..."
+routerAddr=`tcpdump -Ie -i $2 -l -c 100 2> /dev/null | grep "$1" | sed s/.*BSSID:// | sed s/\ \(.*//  | head -n1`
+echo $routerAddr
+
 #sniff for a valid MAC
 # I flag for promisc and e for mac addresses
 # grep for our network ssid then ignore beacons
 echo "Starting listening for 15s"
 tcpdumpBin=`which tcpdump`
-TCPOUT=`tcpdump -Ie -i $2 -c 25 -n dst port 80`
+#TCPOUT=`tcpdump -Ie -i $2 -c 25 -n dst port 80`
 echo $TCPOUT
 echo \n
 echo \n
@@ -38,8 +44,8 @@ echo "Finished Listening"
 echo $MAC
 #spoof the mac from above
 #TODO might have to edit /etc/networks/interface
-sudo ifconfig $2 down hw ether $MAC
-sudo ifconfig $2 up
+#sudo ifconfig $2 down hw ether $MAC
+#sudo ifconfig $2 up
 echo "Changed your mac address!"
 
 # join the ssid
